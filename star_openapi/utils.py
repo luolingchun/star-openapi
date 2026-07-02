ed037d42a97096c01593edd043c94969693e36fa
luolingchun <luolingchun@outlook.com>
2026-07-02 15:40:36 +0800

    Fix `summary` and `description` in `__doc__`

--- star_openapi/utils.py
+++ star_openapi/utils.py
@@ -53,13 +53,9 @@ def get_operation(
     doc = inspect.getdoc(func) or ""
     doc = doc.strip()
     lines = doc.split("\n")
-    doc_summary = lines[0]
 
-    # Determine the summary and description based on provided arguments or docstring
-    if summary is None:
-        doc_description = lines[0] if len(lines) == 0 else "<br/>".join(lines[1:])
-    else:
-        doc_description = "<br/>".join(lines)
+    doc_summary = lines[0]
+    doc_description = "<br/>".join(lines) if summary else "<br/>".join(lines[1:])
 
     summary = summary or doc_summary
     description = description or doc_description
